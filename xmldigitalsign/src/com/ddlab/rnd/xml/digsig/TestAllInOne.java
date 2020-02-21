51d17f3b520f862889890ba2508a95a269335fe8
nehal <nehal@nehal-Inspiron-N5010>
2020-02-21 12:46:34 +0530

    Digital Sign

---
+++ xmldigitalsign/src/com/ddlab/rnd/xml/digsig/TestAllInOne.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ddlab.rnd.xml.digsig;
+
+import com.ddlab.rnd.crypto.KryptoUtil;
+import java.io.File;
+
+public class TestAllInOne {
+
+    public static void main(String[] args) {
+        //Generate the keys
+        String keysDirPath = "keys";
+        KryptoUtil util = new KryptoUtil();
+        util.storeKeyPairs(keysDirPath);
+        System.out.println("Private and Public Keys generated successfully ...");
+        //Sign the XML Dcouments
+        String xmlFilePath = "xml" + File.separator + "employeesalary.xml";
+        String signedXmlFilePath = "xml" + File.separator + "digitallysignedEmpSal.xml";
+        String privateKeyFilePath = "keys" + File.separator + "privatekey.key";
+        String publicKeyFilePath = "keys" + File.separator + "publickey.key";
+        XmlDigitalSignatureGenerator xmlSig = new XmlDigitalSignatureGenerator();
+        xmlSig.generateXMLDigitalSignature(xmlFilePath, signedXmlFilePath, privateKeyFilePath, publicKeyFilePath);
+        //Verify the signed XML Document
+        try {
+            boolean validFlag = XmlDigitalSignatureVerifier.
+                    isXmlDigitalSignatureValid(signedXmlFilePath, publicKeyFilePath);
+            System.out.println("Validity of XML Digital Signature : " + validFlag);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
